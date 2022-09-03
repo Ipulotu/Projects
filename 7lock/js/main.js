f9db914fffa5bcde9d8bfed9a38503b73e6934c2
ipulotu <ipulotu@byui.edu>
2022-09-03 14:51:15 -0600

    fixing json

--- 7lock/js/main.js
+++ 7lock/js/main.js
@@ -1,8 +1,24 @@
 
-const parent = document.querySelector(".parent");
 
 
 fetch("https://ipulotu.github.io/Projects/7lock/data/cards.json")
     .then(response => response.json())
-    .then(data => console.log(data));
-    
+    .then(data => {
+        console.log(data)
+        const parent = document.querySelector(".parent");
+       
+        for (let i = 1; i < 80; i++) {
+            let img = document.createElement("img");
+            img.setAttribute("src", data[i].image);
+            parent.appendChild(img);
+        }
+    });
+
+
+
+
+
+
+
+
+
